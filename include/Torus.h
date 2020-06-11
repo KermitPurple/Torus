adcb9f0ccb30f23a23f7947749ae0616edf7cd68
Shane McDonough <shanemcdman@gmail.com>
2020-06-11 00:11:39 -0700

    Convert the num_of_ variables to ints

--- include/Torus.h
+++ include/Torus.h
@@ -9,9 +9,9 @@ private:
     double radius;
     double smaller_radius;
     double rotation_speed;
-    double num_of_cross_sections;
     double current_offset;
-    double num_of_points;
+    int num_of_cross_sections;
+    int num_of_points;
     void draw_cross_section(Coord position, double angle, double offset_angle);
     std::vector<std::vector<Coord>> points;
 public:
