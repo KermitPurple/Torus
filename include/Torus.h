1cda10b969ac14bd78d43887f9151b9c70546a0f
Shane McDonough <shanemcdman@gmail.com>
2020-06-10 22:03:14 -0700

    Start on Torus::draw

--- include/Torus.h
+++ include/Torus.h
@@ -1,13 +1,15 @@
 #ifndef TORUS_H
 #define TORUS_H
 
+#include<Coord.h>
+
 class Torus{
 private:
     double radius;
     double smaller_radius;
     double rotation_speed;
     double num_of_cross_sections;
-    void draw_cross_section(double position_angle, double offset_angle, int num_of_points);
+    void draw_cross_section(Coord position, double angle, double offset_angle, int num_of_points);
 public:
     Torus();
     void draw();
--- src/Torus.cpp
+++ src/Torus.cpp
@@ -1,9 +1,12 @@
 #include<Torus.h>
 #include<GL/glut.h>
+#include<math.h>
+#include<Coord.h>
+#include<iostream>
 
 // private
 
-void Torus::draw_cross_section(double angle, double offset_angle, int num_of_points){
+void Torus::draw_cross_section(Coord position, double angle, double offset_angle, int num_of_points){
 }
 
 // public
@@ -12,8 +15,17 @@ Torus::Torus(){
     radius = 40;
     smaller_radius = 10;
     rotation_speed = 0.1;
-    num_of_cross_sections = 6;
+    num_of_cross_sections = 10;
 }
 
 void Torus::draw(){
+    glBegin(GL_POINTS);
+    for(double i = 0; i < 2 * M_PI; i += 2 * M_PI / num_of_cross_sections){
+        glVertex3d(
+                cos(i) * radius, 
+                sin(i) * radius,
+                0
+                );
+    }
+    glEnd();
 }
